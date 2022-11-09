16fd55dcbfca45d1ad7c37d45c51a765b2aadc44
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-09 16:46:57 -0300

    Modifico Login

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HysSkillsComponent } from './components/hys-skills/hys-skills.component
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -30,12 +32,14 @@ import { LoginComponent } from './components/login/login.component';
     HysSkillsComponent,
     ProjectsComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
