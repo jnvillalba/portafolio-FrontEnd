77fe874a0b769113e1d511e214e24652c9acf437
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-15 16:28:44 -0300

    agrego nuevos services

---
+++ front-end/src/app/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { JwtDto } from './../model/jwt-dto';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NuevoUsuario } from './../model/nuevo-usuario';
+import { LoginUsuario } from './../model/login-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authURL = 'http://localhost:8080/auth/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+    return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario)
+  }
+
+  public login(loginUsuario: LoginUsuario): Observable<any>{
+    return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario)
+  }
+
+
+}
