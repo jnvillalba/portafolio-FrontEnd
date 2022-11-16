2282f2cd25a3aef68108bbe44b87cb15a2c44e93
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-16 16:30:43 -0300

    experiencia service

---
+++ front-end/src/app/service/experiencia.service.ts
@@ -0,0 +1,33 @@
+import { Experiencia } from './../model/experiencia';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienciaService {
+  expURL = 'http://localhost:8080/explab/'
+
+  constructor(private httpClient: HttpClient ) { }
+
+  public lista(): Observable<Experiencia[]>{
+    return this.httpClient.get<Experiencia[]>(this.expURL + 'lista')
+  }
+
+  public detail(id: number): Observable<Experiencia>{
+    return this.httpClient.get<Experiencia>(this.expURL + `detail/${id}`)
+  }
+
+  public save(exp: Experiencia): Observable<any>{
+    return this.httpClient.post<any>(this.expURL + `create`, exp)
+  }
+
+  public update(id: number, exp: Experiencia): Observable<any>{
+    return this.httpClient.put<any>(this.expURL + `update/${id}`, exp)
+  }
+
+  public delete(id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.expURL + `delete/${id}`)
+  }
+}
