66b7f6347fdfa2a7727deab236f6d61693a0aac1
jnvillalba <joaquinvillalba8@gmail.com>
2023-03-06 19:15:35 -0300

    borro aos

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component,ViewEncapsulation} from '@angular/core';
-import * as AOS from 'aos';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'front-end';
-  ngOnInit() {
-    AOS.init();
-    window.addEventListener('load', AOS.refresh);
-  }
+  ngOnInit() {}
 }
